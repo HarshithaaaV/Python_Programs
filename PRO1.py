adb7088d9c3af7563ac63438d5dd50400e836b95
HarshithaaaV <2004harshithav@gmail.com>
2025-02-17 12:27:50 +0530

    updated

--- PRO1.py
+++ PRO1.py
@@ -1,6 +1,6 @@
 #Write a program to input 2 numbers and print their sum.
 
-num1 = int(input("Enter 1st number: "))
-num2 = int(input("Enter 2nd number: "))
-sum = num1+num2
-print("Sum:",sum)
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+result = num1+num2
+print("The sum of the two numbers is: ", result)
